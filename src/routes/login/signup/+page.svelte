6e97d2c9f63f0a824b02e3e46fad28a94b144912
Ali-Ahmad0 <aliahmadbhs@gmail.com>
2026-01-10 11:25:34 +0500

    changes before deployment

--- src/routes/login/signup/+page.svelte
+++ src/routes/login/signup/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import { SignupCard } from "$lib";
+	import { dev } from "$app/environment";
+
 	let { form } = $props();
 </script>
 
-<SignupCard form={form} />
+{#if dev}
+	<SignupCard form={form} />
+{:else}
+	<h1>Sign up unavailable</h1>
+	<p>Sorry, we don’t support sign-ups on the live hosted version.</p>
+{/if}
